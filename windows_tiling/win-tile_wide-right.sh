01a8cd62ffe31b9a42d00395f91d9da77476dd10
Philip Whiteside <pwsubbox@gmail.com>
2019-03-12 06:10:12 +0000

    Add windows tiling wide layouts

---
+++ windows_tiling/win-tile_wide-right.sh
@@ -0,0 +1,5 @@
+wmctrl -r :ACTIVE: -b remove,fullscreen
+wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz
+# wmctrl -r :ACTIVE: -e unknown,left-offset,top-offset,width,height
+wmctrl -r :ACTIVE: -e 0,900,0,2540,0
+wmctrl -r :ACTIVE: -b add,maximized_vert
